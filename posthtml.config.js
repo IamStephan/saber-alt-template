d11cd02790e88011d621e5e2b7d6608cb6b6dd14
iamstephan <stephanburger54@gmail.com>
2022-01-05 14:09:47 +0200

    using js config

---
+++ posthtml.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    "posthtml-pipeline": {
+      plugins: [
+        require("posthtml-extends").default(),
+        require("posthtml-tokens").default({
+          tokens: {
+            "[[hello]]": "WHIIIIIOOOOO",
+            "[[title]]": "Template Project",
+          },
+        }),
+      ],
+    },
+  },
+};
