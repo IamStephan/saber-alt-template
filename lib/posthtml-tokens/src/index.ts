27f0040700aad3beda74157e2af31615324a1879
iamstephan <stephanburger54@gmail.com>
2022-01-05 14:18:17 +0200

    simplified logic

--- lib/posthtml-tokens/src/index.ts
+++ lib/posthtml-tokens/src/index.ts
@@ -1,18 +1,36 @@
-import "core-js";
-import { Node } from "posthtml";
-import { merge } from "lodash";
-import transformTokens from "./transformTokens";
-import { IOptions } from "./types/options.d";
+import 'core-js'
+import { merge } from 'lodash'
+import { parser as parseHtml } from 'posthtml-parser'
+import { render as renderHtml, Node as RenderNode } from 'posthtml-render'
+import { IOptions } from './types/options'
+import { IExtendedNode } from './types/node'
 
-const PostHTMLTokens = (options: IOptions) => (tree: Node) => {
-  let _tree = tree;
+const PostHTMLTokens = (options: IOptions) => (tree: IExtendedNode) => {
+  let _tree = tree
+  let isAltered = false
+  let NodeString = renderHtml(_tree as RenderNode)
 
-  const _globalTokens = merge({}, options.tokens ?? {});
-  _tree = transformTokens(_tree, _globalTokens) as Node;
+  const _globalTokens = merge({}, options.tokens ?? {})
 
-  return _tree;
-};
+  for (let [token, value] of Object.entries(_globalTokens)) {
+    if (NodeString.includes(token)) {
+      isAltered = true
+      if (typeof value === 'function') {
+        const _value = value()
+        NodeString = NodeString.replaceAll(token, _value)
+      } else {
+        NodeString = NodeString.replaceAll(token, value)
+      }
+    }
+  }
 
-// TODO: Make use of `tree.messages.push` to indicate if changes took place
+  _tree = parseHtml(NodeString) as any
+
+  return {
+    tree: _tree,
+    isAltered,
+  }
+}
+
 // TODO: Add ability to handle namespaced tokens
-export default PostHTMLTokens;
+export default PostHTMLTokens
