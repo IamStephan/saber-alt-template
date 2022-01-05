053751d69a103d264ada8810bed426cea1da6872
iamstephan <stephanburger54@gmail.com>
2022-01-05 14:19:53 +0200

    simplified and now matches pipeline logic

--- lib/posthtml-extends/src/index.ts
+++ lib/posthtml-extends/src/index.ts
@@ -1,10 +1,10 @@
 import { Node } from "posthtml";
 import { merge } from "lodash";
+
 import { IOptions } from "./types/options";
 import { TemplateUseTags } from "./constants/useTags";
-
-import handleUseTag from "./tags/use";
-import handleTemplateTags from "./tags/template";
+import _handleSlotFill from "./tags/slotFill";
+import _handleUseTags from "./tags/use";
 
 const DefaultOptions: Partial<IOptions> = {
   useTags: {
@@ -14,17 +14,14 @@ const DefaultOptions: Partial<IOptions> = {
 
 const PostHTMLExtends = (options: IOptions) => (tree: Node) => {
   const mergedOptions = merge(DefaultOptions, options);
-  let _tree = tree;
 
-  // Build the entire tree
-  _tree = handleUseTag(_tree, mergedOptions);
+  const { tree: _tree, isAltered } = _handleUseTags(tree, mergedOptions);
 
-  // Handle template definitions
-  _tree = handleTemplateTags(_tree, mergedOptions);
-
-  return _tree;
+  return {
+    tree: _tree,
+    isAltered,
+  };
 };
 
-// TODO: Make use of `tree.messages.push` to indicate if changes took place
 // TODO: correct relative imports first before inserting into the main AST
 export default PostHTMLExtends;
