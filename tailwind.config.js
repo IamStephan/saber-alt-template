cc10a90adaa7355388b72b9aa6c70936bdb81cba
iamstephan <stephanburger54@gmail.com>
2022-01-06 14:42:57 +0200

    using presets and code format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{html,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
+  content: ['./src/**/*.{html,css}'],
+  presets: [
+    require('./lib/tailwindcss-preset-colors'),
+    require('./lib/tailwindcss-preset-screens'),
   ],
-};
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
