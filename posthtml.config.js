9b015d829a728a2833c3bab5cbbb3e57a3ecdfcc
iamstephan <stephanburger54@gmail.com>
2022-01-07 12:07:29 +0200

    added html tokens

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const fs = require('fs')
+
 module.exports = {
   plugins: {
     'posthtml-pipeline': {
@@ -5,8 +8,44 @@ module.exports = {
         require('posthtml-extends').default(),
         require('posthtml-tokens').default({
           tokens: {
-            '[[hello]]': 'WHIIIIIOOOOO',
             '[[title]]': 'Template Project',
+            '[[page_name]]': 'Content Header',
+            '[[city_scroll:50]]': () => {
+              const pathToToken = path.resolve(
+                process.cwd(),
+                './src/tokens/city_scroll.html'
+              )
+              return fs.readFileSync(pathToToken, 'utf8')
+            },
+
+            '[[breadcrumbs]]': () => {
+              const pathToToken = path.resolve(
+                process.cwd(),
+                './src/tokens/breadcrumbs.html'
+              )
+              return fs.readFileSync(pathToToken, 'utf8')
+            },
+            '[[breadcrumbs]]': () => {
+              const pathToToken = path.resolve(
+                process.cwd(),
+                './src/tokens/breadcrumbs.html'
+              )
+              return fs.readFileSync(pathToToken, 'utf8')
+            },
+            '[[single_silo_nav]]': () => {
+              const pathToToken = path.resolve(
+                process.cwd(),
+                './src/tokens/single_silo_nav.html'
+              )
+              return fs.readFileSync(pathToToken, 'utf8')
+            },
+            '[[content]]': () => {
+              const pathToToken = path.resolve(
+                process.cwd(),
+                './src/tokens/content.html'
+              )
+              return fs.readFileSync(pathToToken, 'utf8')
+            },
           },
         }),
       ],
