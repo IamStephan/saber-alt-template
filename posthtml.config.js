93518c0b23935bc1a0a42c63b644d8659e7ee0d6
iamstephan <stephanburger54@gmail.com>
2022-01-11 16:13:52 +0200

    config to match token scopes

--- posthtml.config.js
+++ posthtml.config.js
@@ -5,18 +5,23 @@ module.exports = {
         require('posthtml-extends').default(),
         require('posthtml-tokens').default({
           tokens: {
-            '[[title]]': 'Template Project',
-            '[[page_name]]': 'Content Header',
-            '[[city_scroll:50]]': ({ readFile }) =>
-              readFile('./src/tokens/city_scroll.html'),
-            '[[breadcrumbs]]': ({ readFile }) =>
-              readFile('./src/tokens/breadcrumbs.html'),
-            '[[breadcrumbs]]': ({ readFile }) =>
-              readFile('./src/tokens/breadcrumbs.html'),
-            '[[single_silo_nav]]': ({ readFile }) =>
-              readFile('./src/tokens/single_silo_nav.html'),
-            '[[content]]': ({ readFile }) =>
-              readFile('./src/tokens/content.html'),
+            global: {
+              '[[title]]': 'Template Project',
+              '[[page_name]]': 'Content Header',
+              '[[city_scroll:50]]': ({ readFile }) =>
+                readFile('./src/tokens/city_scroll.html'),
+              '[[breadcrumbs]]': ({ readFile }) =>
+                readFile('./src/tokens/breadcrumbs.html'),
+              '[[breadcrumbs]]': ({ readFile }) =>
+                readFile('./src/tokens/breadcrumbs.html'),
+              '[[single_silo_nav]]': ({ readFile }) =>
+                readFile('./src/tokens/single_silo_nav.html'),
+              '[[content]]': ({ readFile }) =>
+                readFile('./src/tokens/content.html'),
+            },
+            test: {
+              '[[title]]': 'Altered',
+            },
           },
         }),
       ],
