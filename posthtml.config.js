bb63472d9ccf3320abce6c9c7aea8b9ec97a7dc7
iamstephan <stephanburger54@gmail.com>
2022-01-31 12:36:35 +0200

    new token definitions

--- posthtml.config.js
+++ posthtml.config.js
@@ -8,6 +8,8 @@ module.exports = {
             global: {
               '[[title]]': 'Template Project',
               '[[page_name]]': 'Content Header',
+              '[phone]': '1-800-922-2488',
+              '[license number]': 'Contractor License #826234',
               '[[city_scroll:50]]': ({ readFile }) =>
                 readFile('./src/tokens/city_scroll.html'),
               '[[breadcrumbs]]': ({ readFile }) =>
@@ -18,9 +20,12 @@ module.exports = {
                 readFile('./src/tokens/single_silo_nav.html'),
               '[[content]]': ({ readFile }) =>
                 readFile('./src/tokens/content.html'),
-            },
-            test: {
-              '[[title]]': 'Altered',
+              '[[top_nav]]': ({ readFile }) =>
+                readFile('./src/tokens/top_nav.html'),
+              '[[bottom_nav]]': ({ readFile }) =>
+                readFile('./src/tokens/bottom_nav.html'),
+              '[[display_addresses]]': ({ readFile }) =>
+                readFile('./src/tokens/display_addresses.html'),
             },
           },
         }),
